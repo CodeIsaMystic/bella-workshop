97890dbbd09f342f63d0139025b45c7a8ec96fa4
Julien Orjollet <oj.professionnel@gmail.com>
2020-11-11 13:11:40 +0100

    ending moveImages function for left & right parts of the screen

--- 00-Full-Page/js/main.js
+++ 00-Full-Page/js/main.js
@@ -50,10 +50,42 @@ function initHeaderTilt() {
 
 function moveImages(e) {
     //console.log(e);
+
     const { offsetX, offsetY, target } = e;
     const { clientWidth, clientHeight } = target;
+    //console.log(offsetX, offsetY, clientWidth, clientHeight);
 
-    console.log(offsetX, offsetY, clientWidth, clientHeight);
+    const xPos = (offsetX / clientWidth) - 0.5;
+    const yPos = (offsetY / clientHeight) - 0.5;
+
+    const leftImages = gsap.utils.toArray('.hg__left .hg__image');
+    const rightImages = gsap.utils.toArray('.hg__right .hg__image');
+    const modifier = (index) => index * 1.2 + 0.5;
+
+    /** Move left 3 images  **/
+    leftImages.forEach((image, index) => {
+        gsap.to(image, {
+            duration: 1.2,
+            x: xPos * 20 * modifier(index),
+            y: yPos * 30 * modifier(index),
+            rotationY: xPos * 25,
+            rotationX: yPos * 10,
+            ease: 'power3.out'
+        });
+    });
+
+    /** Move right 3 images  **/
+    rightImages.forEach((image, index) => {
+        gsap.to(image, {
+            duration: 1.2,
+            x: -xPos * 20 * modifier(index),
+            y: yPos * 30 * modifier(index),
+            rotationY: xPos * 25,
+            rotationX: yPos * 10,
+            ease: 'power3.out'
+        });
+    });
+
 }
 
 
